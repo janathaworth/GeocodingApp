856fb415e9c1ef967c8d82b7756abfb0c7139bb8
Connie Jehng <conniejehng@wireless-10-145-73-82.public.utexas.edu>
2018-11-01 19:39:46 -0500

    working latitude and longitude

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -21,11 +21,13 @@ import android.widget.Toast;
 
 import com.android.volley.Request.Method;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,28 +45,48 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user clicks the Send button */
     public void sendMessage(View view){
         // Do something in response to button
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText =(EditText) findViewById(R.id.edit_message);
-//        String message = editText.getText().toString();
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText =(EditText) findViewById(R.id.edit_message);
+        String location = editText.getText().toString();
+        location = location.replace(" ", "+");
 //        intent.putExtra(EXTRA_MESSAGE, message);
 //        startActivity(intent);
+        RequestQueue queue = Volley.newRequestQueue(this);
 
-        String url = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyB3R3NhOdAkzw6m-lu2OObbMA8NBQ1_5mw";
+        // String url = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyB3R3NhOdAkzw6m-lu2OObbMA8NBQ1_5mw";
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyB3R3NhOdAkzw6m-lu2OObbMA8NBQ1_5mw";
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        txtCoord.setText("Response: " + response.toString());
+                        // txtCoord.setText("Response: " + response.toString());
+
+                        try {
+                            JSONObject resultContents = ((JSONArray) response.get("results")).getJSONObject(0).getJSONObject("geometry");
+                            String lat = resultContents.getJSONObject("location").get("lat").toString();
+                            String lon = resultContents.getJSONObject("location").get("lng").toString();
+                            txtCoord.setText("latitude: " + lat  + "\nlongitude: " + lon);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }, new Response.ErrorListener() {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // TODO: Handle error
+                        System.out.println("error" + error);
 
                     }
                 });
+
+//        String message = "message";
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+
+        queue.add(jsonObjectRequest);
     }
 }
